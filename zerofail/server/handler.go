7aa09b9ebd6f41ed55bf3c96a65399731204e6da
highonsemicolon <onkar.chendage@gmail.com>
2025-07-08 04:38:05 +0000

    add upsert handler

--- zerofail/server/handler.go
+++ zerofail/server/handler.go
@@ -2,87 +2,72 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	proto "github.com/highonsemicolon/experiments/zerofail/proto"
-
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type RecordServiceServer struct {
 	proto.UnimplementedRecordServiceServer
 }
 
-func (s *RecordServiceServer) InsertRecords(ctx context.Context, req *proto.InsertRequest) (*proto.InsertResponse, error) {
+func (s *RecordServiceServer) UpsertRecords(ctx context.Context, req *proto.UpsertRequest) (*proto.UpsertResponse, error) {
 
-	session, err := Client.StartSession()
-	if err != nil {
-		return nil, err
-	}
-	defer session.EndSession(ctx)
-
-	err = mongo.WithSession(ctx, session, func(sc mongo.SessionContext) error {
-		if err := session.StartTransaction(); err != nil {
-			return err
+	// input validation
+	col1Set := make(map[string]bool)
+	col2Set := make(map[string]bool)
+	for _, r := range req.Records {
+		if col1Set[r.Col1] || col2Set[r.Col2] {
+			return &proto.UpsertResponse{
+				Message: "Duplicate col1 or col2 in request itself",
+				Success: false,
+			}, nil
 		}
-
-		var docs []any
-		for _, r := range req.Records {
-			docs = append(docs, bson.M{"col1": r.Col1, "col2": r.Col2, "createdAt": time.Now(), "deleted": false})
-		}
-
-		if _, err := RecordCollection.InsertMany(sc, docs); err != nil {
-			_ = session.AbortTransaction(sc)
-			return err
-		}
-
-		return session.CommitTransaction(sc)
-	})
-
-	if err != nil {
-		return &proto.InsertResponse{Message: "Insert failed: " + err.Error(), Success: false}, nil
+		col1Set[r.Col1] = true
+		col2Set[r.Col2] = true
 	}
 
-	return &proto.InsertResponse{Message: "Insert successful", Success: true}, nil
-}
+	now := time.Now()
+	var models []mongo.WriteModel
+	var idsToCleanup []string
 
-func (s *RecordServiceServer) DeleteRecords(ctx context.Context, req *proto.DeleteRequest) (*proto.DeleteResponse, error) {
-	session, err := Client.StartSession()
-	if err != nil {
-		return nil, err
-	}
-	defer session.EndSession(ctx)
+	for i, r := range req.Records {
+		docID := fmt.Sprintf("%s_%02d", req.OrderID, i)
+		idsToCleanup = append(idsToCleanup, docID)
 
-	err = mongo.WithSession(ctx, session, func(sc mongo.SessionContext) error {
-		if err := session.StartTransaction(); err != nil {
-			return err
-		}
+		upsert := mongo.NewReplaceOneModel().
+			SetFilter(bson.M{"_id": docID}).
+			SetReplacement(bson.M{
+				"_id":       docID,
+				"col1":      r.Col1,
+				"col2":      r.Col2,
+				"createdAt": now,
+			}).
+			SetUpsert(true)
 
-		for _, r := range req.Records {
-			filter := bson.M{"col1": r.Col1, "col2": r.Col2}
-			var record bson.M
-			err := RecordCollection.FindOneAndDelete(sc, filter).Decode(&record)
-			if err != nil {
-				_ = session.AbortTransaction(sc)
-				return err
-			}
-
-			record["deletedAt"] = time.Now()
-			record["deletedBy"] = req.DeletedBy
-
-			if _, err := DeletedCollection.InsertOne(sc, record); err != nil {
-				_ = session.AbortTransaction(sc)
-				return err
-			}
-		}
-
-		return session.CommitTransaction(sc)
-	})
-
-	if err != nil {
-		return &proto.DeleteResponse{Message: "Delete failed: " + err.Error(), Success: false}, nil
+		models = append(models, upsert)
 	}
 
-	return &proto.DeleteResponse{Message: "Delete successful", Success: true}, nil
+	opts := options.BulkWrite().SetOrdered(false)
+	_, err := RecordCollection.BulkWrite(ctx, models, opts)
+	if err != nil {
+		// on any failure, rollback all
+		_, _ = RecordCollection.DeleteMany(ctx, bson.M{
+			"_id": bson.M{"$in": idsToCleanup},
+		})
+
+		return &proto.UpsertResponse{
+			Success: false,
+			Message: "Upsert failed: " + err.Error(),
+		}, nil
+	}
+
+	return &proto.UpsertResponse{
+		Success: true,
+		Message: "Upsert successful",
+	}, nil
 }
