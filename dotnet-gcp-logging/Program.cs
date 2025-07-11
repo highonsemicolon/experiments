2b503ee74110ee8deddf3ae34fcdd0aeaf0b7fd1
highonsemicolon <onkar.chendage@gmail.com>
2025-07-11 17:27:30 +0000

    feat: add google cloud logging support

--- dotnet-gcp-logging/Program.cs
+++ dotnet-gcp-logging/Program.cs
@@ -1,6 +1,11 @@
 using dotnet_gcp_logging;
+using Google.Cloud.Logging.Console;
 
 Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(logging => {
+        logging.ClearProviders();
+        logging.AddGoogleCloudConsole();
+    })
     .ConfigureServices(services => {
         services.AddHostedService<Worker>();
     })
