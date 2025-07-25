c7100b5f648b72dcc61f61d7635aa0037c5a31c1
highonsemicolon <onkar.chendage@gmail.com>
2025-07-25 16:58:26 +0000

    add xml to c# object to json

---
+++ xmlParser/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using System.Xml.Serialization;
+
+
+string xmlPath = "sample.xml";
+var options = new JsonSerializerOptions { WriteIndented = true };
+
+// Deserialize XML into a Person object
+var serializer = new XmlSerializer(typeof(Person));
+using var reader = new StreamReader(xmlPath);
+var person = (Person)serializer.Deserialize(reader)!;
+
+// Convert the object to JSON using System.Text.Json
+var json = JsonSerializer.Serialize(person, options);
+
+Console.WriteLine(json);
+
