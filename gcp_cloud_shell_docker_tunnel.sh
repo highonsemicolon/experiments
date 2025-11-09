2ae682c1520be1b1b2d7346da4a1c49c0b2dbcff
onkar <onkar.chendage@gmail.com>
2025-11-09 22:19:07 +0530

    port-forward port 8000 by default

--- gcp_cloud_shell_docker_tunnel.sh
+++ gcp_cloud_shell_docker_tunnel.sh
@@ -20,5 +20,5 @@ EOF
     fi
 "'
 
-gcloud cloud-shell ssh --authorize-session --ssh-flag="-L 2375:localhost:2375"
+gcloud cloud-shell ssh --authorize-session --ssh-flag="-L 8000:localhost:8000" --ssh-flag="-L 2375:localhost:2375"
 
