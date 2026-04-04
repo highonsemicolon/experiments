23ccea37b94e585a015fc004d9dd64724a2d68e4
onkar <onkar.chendage@gmail.com>
2026-04-04 17:10:43 +0530

    feat: ensures mysql always interprets times as UTC

--- appointment-booking/config/config.go
+++ appointment-booking/config/config.go
@@ -1,5 +1,7 @@
 package config
 
+import "fmt"
+
 type Config struct {
 	DBHost     string `koanf:"db.host" validate:"required"`
 	DBPort     string `koanf:"db.port" validate:"required"`
@@ -7,4 +9,10 @@ type Config struct {
 	DBPassword string `koanf:"db.password" validate:"required"`
 	DBName     string `koanf:"db.name" validate:"required"`
 	ServerPort string `koanf:"server.port" validate:"required"`
+}
+
+func (c *Config) DSN() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=UTC",
+		c.DBUser, c.DBPassword, c.DBHost, c.DBPort, c.DBName,
+	)
 }
