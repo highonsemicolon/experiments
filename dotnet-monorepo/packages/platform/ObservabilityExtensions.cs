62fdb000c8ef1d6edbe3e58cc403240b7e3b1e73
highonsemicolon <onkar.chendage@gmail.com>
2026-04-27 05:03:35 +0000

    refactor: enable HostApplicationBuilder support for observability and add ActivitySource tracking to greeterClient

--- dotnet-monorepo/packages/platform/ObservabilityExtensions.cs
+++ dotnet-monorepo/packages/platform/ObservabilityExtensions.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 using Platform.Logging;
@@ -11,6 +10,17 @@ public static class ObservabilityExtensions {
     public static WebApplicationBuilder AddPlatformObservability(
         this WebApplicationBuilder builder) {
         builder.AddPlatformLogging();
+
+        builder.Services.AddPlatformTelemetry(builder.Environment);
+        builder.Services.AddPlatformCorrelation();
+
+        return builder;
+    }
+
+    public static HostApplicationBuilder AddPlatformObservability(
+        this HostApplicationBuilder builder) {
+        builder.AddPlatformLogging();
+
         builder.Services.AddPlatformTelemetry(builder.Environment);
         builder.Services.AddPlatformCorrelation();
 
