e3861c0d98d2526d844f865cdc1bf6af4c5341c4
highonsemicolon <onkar.chendage@gmail.com>
2026-05-03 07:31:43 +0000

    feat: inject AppSettings and log greeter URL in worker service

--- dotnet-monorepo/services/greetWorker/Worker.cs
+++ dotnet-monorepo/services/greetWorker/Worker.cs
@@ -1,5 +1,5 @@
 using System.Diagnostics;
-
+using Microsoft.Extensions.Options;
 using Greeter.V1;
 
 using Platform.Telemetry;
@@ -10,15 +10,19 @@ public class Worker : BackgroundService {
     private readonly ILogger<Worker> _logger;
     private readonly GreeterService.GreeterServiceClient _greeterClient;
     private readonly ActivitySource _activitySource;
+    private readonly AppSettings _settings;
 
 
-    public Worker(ILogger<Worker> logger, GreeterService.GreeterServiceClient greeterClient, IActivitySourceFactory factory) {
+    public Worker(ILogger<Worker> logger, IOptions<AppSettings>options, GreeterService.GreeterServiceClient greeterClient, IActivitySourceFactory factory) {
         _logger = logger;
         _greeterClient = greeterClient;
         _activitySource = factory.Create<Worker>();
+        _settings = options.Value;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+        _logger.LogInformation("Greeter URL: {Url}", _settings.Greeter.Url);
+        
         while (!stoppingToken.IsCancellationRequested) {
             using var activity = _activitySource.StartActivity("message-loop", ActivityKind.Consumer);
             await SendGreeting(stoppingToken);
