17f9e8151838e7ddc56766d7085ce62fd5210a55
onkar <onkar.chendage@gmail.com>
2026-05-30 23:18:32 +0530

    fix cors

--- auth/server/server.js
+++ auth/server/server.js
@@ -1,14 +1,23 @@
 const express = require('express')
-const app = express()
 const { auth } = require('express-oauth2-jwt-bearer')
+const cors = require('cors')
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8081
 
 const jwtCheck = auth({
   audience: 'pathfinder-backend',
   issuerBaseURL: 'https://pathfinder-1.us.auth0.com/',
   tokenSigningAlg: 'RS256',
 })
+
+const app = express()
+
+app.use(cors({
+  origin: 'http://localhost:8080',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 app.get('/', function (req, res) {
   res.send('welcome')
